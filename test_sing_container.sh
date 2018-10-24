32c172113529e717cd96a15bcd73314018395889
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2018-10-24 18:28:34 -0500

    Ready for first test container

---
+++ test_sing_container.sh
@@ -0,0 +1,16 @@
+
+singularity \
+run \
+--bind INPUTS:/INPUTS \
+--bind OUTPUTS:/OUTPUTS \
+baxpr-fmri_modularity-master-v1.0.0.simg \
+magick_path /usr/bin \
+community_file eight_networks_tcorr05_2level_43_0840rois_3study.csv \
+connmat_file ../INPUTS/connmat.csv \
+roiinfo_file ../INPUTS/roiinfo.csv \
+roi_file ../INPUTS/roi.nii.gz \
+project UNK_PROJ \
+subject UNK_SUBJ \
+session UNK_SESS \
+scan UNK_SCAN \
+out_dir /OUTPUTS
