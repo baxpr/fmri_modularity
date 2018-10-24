9d5230bdef9d4f1352fc0d4990698b84e30d444b
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2018-10-24 18:20:37 -0500

    Compiled matlab working

---
+++ bin/test_compiled_matlab.sh
@@ -0,0 +1,12 @@
+
+sh run_fmri_modularity.sh /usr/local/MATLAB/MATLAB_Runtime/v92 \
+magick_path /usr/bin \
+community_file eight_networks_tcorr05_2level_43_0840rois_3study.csv \
+connmat_file ../INPUTS/connmat.csv \
+roiinfo_file ../INPUTS/roiinfo.csv \
+roi_file ../INPUTS/roi.nii.gz \
+project UNK_PROJ \
+subject UNK_SUBJ \
+session UNK_SESS \
+scan UNK_SCAN \
+out_dir ../OUTPUTS
